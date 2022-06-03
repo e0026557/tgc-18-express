84e8adbc17c0aaded6ca68daf20662b68b66f8ab
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-03 03:16:53 +0000

    Added files

--- 04-template-inheritance/index.js
+++ 04-template-inheritance/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const hbs = require('hbs');
+const waxOn = require('wax-on'); // wax-on provides template inheritance 
 
 const app = express();
 app.set('view engine', 'hbs'); // set the view engine to hbs
 app.use(express.static('public')); // the public folder will hold our static files
+
+// Set up wax-on
+waxOn.on(hbs.handlebars);
+// Tell wax-on where to find the base layouts (layouts = template)
+waxOn.setLayoutPath('./views/layouts');
 
 app.get('/', function(req, res) {
     res.send('Hello world');
