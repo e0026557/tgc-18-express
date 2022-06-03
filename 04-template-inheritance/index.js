04682868bfa59397bb2399163bd6c4ec6eb8764b
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-03 06:09:41 +0000

    Added new files

--- 04-template-inheritance/index.js
+++ 04-template-inheritance/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { handlebars } = require('hbs');
 const hbs = require('hbs');
 const waxOn = require('wax-on'); // wax-on provides template inheritance 
 
@@ -11,6 +12,24 @@ waxOn.on(hbs.handlebars);
 // Tell wax-on where to find the base layouts (layouts = template)
 waxOn.setLayoutPath('./views/layouts');
 
+
+// Register our custom helpers
+// ifEquals
+// First arg -> name of helper
+// Second arg -> callback function
+// -> The callback function has 3 arg
+// -> arg1 and arg2 are the data from the hbs
+// -> arg3 is the options
+hbs.handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+    if (arg1 == arg2) {
+        options.fn(this); // ask handlebars to execute the line inside the ifEquals block in the hbs file ('this' refers to hbs)
+    }
+    else {
+        options.inverse(this); // execute the line in the else block
+    }
+})
+
+// ROUTES
 app.get('/', function(req, res) {
     res.send('Hello world');
 }) 
