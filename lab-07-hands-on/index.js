8638ece6b042ad8e17fb94b5d88ec28c50fa179d
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-06 08:33:57 +0000

    Added notes

--- lab-07-hands-on/index.js
+++ lab-07-hands-on/index.js
@@ -12,6 +12,7 @@ wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
 
 // Set up express to process forms
+// -> Note: If this is not done, req.body will be undefined
 app.use(express.urlencoded({
     'extended': false // use extended : true if you are processing objects in objects in a form
 }))
@@ -32,6 +33,8 @@ app.get('/', async function(req, res) {
 })
 
 // Create new movie entry
+// Note: convention is to use /<noun>/<verb>
+// Eg: /movies/create
 app.get('/create-movie', function(req, res) {
     res.render('create-movie-form.hbs');
 })
@@ -40,12 +43,21 @@ app.get('/create-movie', function(req, res) {
 app.post('/create-movie', async function(req, res) {
     let url = BASE_API_URL + 'movie/create';
     
+    // 'body' or 'payload' in API documentation refers to the data object that we are going to post to the API endpoint
+
+    // Note: the keys in the 'data' object must match that required from the API
     let data = {
         title: req.body.title,
         plot: req.body.plot
-    }
+    };
     
     await axios.post(url, data);
+
+    // Alternatively:
+    // await axios.post(url, {
+    //     title: req.body.title,
+    //     plot: req.body.plot
+    // });
     
     res.redirect('/');
 })
